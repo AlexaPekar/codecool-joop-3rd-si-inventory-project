0c7d6258ff3b8d476517fab03f13348abc415115
AlexaPekar <missdreambelle@gmail.com>
2018-02-01 19:47:35 +0100

    Implemented StoreManager class and all of its required methods

--- src/main/java/com/codecool/StoreManager.java
+++ src/main/java/com/codecool/StoreManager.java
@@ -1,24 +1,43 @@
 package com.codecool;
 
+import java.util.List;
+
 public class StoreManager {
+
+    private  StorageCapable storage;
 
     public void addStorage(StorageCapable storage) {
 
+        this.storage = storage;
     }
 
     public void addCDProduct(String name, int price, int tracks) {
-
+        storage.storeCDProducts(name, price, tracks);
     }
 
     public void addBookProduct(String name, int price, int pages) {
-
+        storage.storeBookProduct(name, price, pages);
     }
 
     public String listProducts() {
-        return null;
+        String listedProducts = "";
+        List<Product> products = storage.getAllProduct();
+        for (int i = 0; i < products.size(); i++) {
+            if (i != products.size()-1) {
+                listedProducts += products.get(i).getName() + ", ";
+            } else {
+                listedProducts += products.get(i).getName();
+            }
+        }
+        return listedProducts;
     }
 
     public int getTotalProductPrice() {
-        return 0;
+        int totalPrice = 0;
+        List<Product> products = storage.getAllProduct();
+        for (int i = 0; i < products.size(); i++) {
+            totalPrice += products.get(i).getPrice();
+        }
+        return totalPrice;
     }
 }
